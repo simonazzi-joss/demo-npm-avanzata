8181c011f3e8c0ca2cf65575c0b8f6110c8019d6
simonazzi joss <simonazzijoss@gmail.com>
2018-02-21 16:42:26 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
+	"use strict";
 	return gutil.log('Gulp è in esecuzione');
 });
 
 gulp.task('copiaHtml', function() {
+	"use strict";
 	gulp.src('source/html/*.html').pipe(gulp.dest('dist'));
 	//gulp.src('source/javascript/*.js').pipe(gulp.dest('dist'));
 	return gulp.src('source/javascript/*.js')
@@ -17,4 +20,10 @@ gulp.task('copiaHtml', function() {
 			.pipe(uglify())
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/'));
+});
+gulp.task('jshint', function() {
+	"use strict";
+	return gulp.src('source/javascript/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
 });
